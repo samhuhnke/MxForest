44051f7eb9950b91466abfa427180470c5a32443
Sam Huhnke <sam.huhnke@web.de>
2023-08-09 17:12:21 +0200

    added/cleaned variables usable to filter by site and tree number for 04 and 09

--- MxForest.R
+++ MxForest.R
@@ -11,7 +11,7 @@ library(tidyr)      #data cleaning
 library(stringr)    #data cleaning
 
 
-# tree inventory data from 2004 - 2020 
+# tree inventory data from 2004 - 2020 --------------------------------------------
 
 ## 2004 - 2007
 Raw.04 <- fread(here("data", "arbolado", "INFyS_Arbolado_2004_2007.csv"))
@@ -30,7 +30,7 @@ str(Raw.14)
 
 
 
-# assinging new ds for warngling
+# assinging new ds for warngling --------------------------------------------------
 
 Arb.04 <- Raw.04
 
@@ -45,6 +45,8 @@ Arb.14 <- Raw.14
 View(Arb.14)
 
 
+# normalizing clnames and comparable variables --------------------------------------
+
 # adapting Arb.04 & Arb.09 variable names
 
 colnames(Arb.04) == colnames(Arb.09) # Varbiablename inconsistent --> forma biologica
@@ -53,19 +55,18 @@ colnames(Arb.04) == colnames(Arb.09) # Varbiablename inconsistent --> forma biol
 Arb.09 <- Arb.09 %>%  rename(forma_biologica = Forma_Biologica_1)
 
 
-# adapting Arb.14 to other datasets
 
-
-
-
-
-
-
-# removing entry mistakes
+# removing entry mistakes ----------------------------------------------------------
 
 ## Estado - should be 32 distinct values
 ## Registro - counting individual entries per Site per 'Conglomerado'
 
+
+# Arb.04 ---------------------------------------------------------
+
+### Arb.04 - quick-arrange to check stuff easier
+Arb.04 <- Arb.04 %>%  
+  arrange(Estado, Conglomerado, Sitio, Registro)
 
 ### Arb.04 - Estado - initially 33
 Arb.04 <- Arb.04 %>% 
@@ -76,17 +77,23 @@ Arb.04 <- Arb.04 %>%
 
 View(Arb.04)
 
-
 ### Arb.04 - Registro - initially all values 'NULL'
 Arb.04 <- Arb.04 %>% 
-  mutate(Registro = str_sub(string = cgl_sit_arb, -1)) %>% 
-  select(Estado, Conglomerado, Sitio, Registro, everything())
-
+  mutate(Registro = str_extract(string = cgl_sit_arb, "(?<=_)[^_]+$") %>% #extracting values out of (somehow complete) strings
+  as.integer(Registro)) %>% 
+  select(Estado, Conglomerado, Sitio, Registro, everything()) 
 
 View(Arb.04)
 
 
-### Arb.09 - initially 38
+
+
+
+
+
+
+
+### Arb.09 - Estado - initially 38
 Arb.09 <- Arb.09 %>% 
   mutate(Estado = case_when(Estado == "Distrito Federal" ~ "Ciudad de México",
                             Estado == "Michoacan de Ocampo" ~ "Michoacán de Ocampo",
@@ -99,6 +106,31 @@ Arb.09 <- Arb.09 %>%
   arrange(Estado, Conglomerado, Sitio, Registro)
 
 View(Arb.09)
+
+
+### Arb.09 - Registro -  Mistake in Guerrero 69336 2: 316 - supposedly just 16
+### Arb.09 - cgl_sit_arb - same issue in string of Conglomerado, Sitio and Registro
+Test.09 <- Arb.09 %>% 
+  mutate(Registro = case_when(Registro == 316 ~ 16,
+                              TRUE ~ Registro)) %>% 
+  mutate(cgl_sit_arb = str_replace(string = cgl_sit_arb, "316$", "16"))
+
+
+### Arb.09 - Registro - check
+Test.09 <- Arb.09 %>% 
+  distinct(Registro)
+
+View(Test.09)
+
+### Arb.09 - Registro - Mistake in Guerrero 69336 2: 316 - supposedly just 16
+Test.09 <- Arb.09 %>% 
+  mutate(Registro = case_when(Registro == 316 ~ 16,
+                              TRUE ~ Registro)) %>% 
+  mutate(cgl_sit_arb = str_replace(string = cgl_sit_arb, "316$", "16"))
+
+
+
+
 
 
 ### Arb.14 - initially 32 
