3a98db0bec33fa177fcc5a774424c9910e445a82
Sam Huhnke <sam.huhnke@web.de>
2023-08-12 19:43:07 +0200

    added cleaning of 'Estado' and 'Registro' for Arb.04 and 09

--- MxForest.R
+++ MxForest.R
@@ -45,45 +45,43 @@ Arb.14 <- Raw.14
 View(Arb.14)
 
 
-# normalizing clnames and comparable variables --------------------------------------
+##----------------------------------------------------------------------------------------------------------------
+## Arb.04 Data cleaning ------------------------------------------------------------------------------------------
 
-# adapting Arb.04 & Arb.09 variable names
-
-colnames(Arb.04) == colnames(Arb.09) # Varbiablename inconsistent --> forma biologica
-
-## adapt name in Arb.09 to "forma_biologica"
-Arb.09 <- Arb.09 %>%  rename(forma_biologica = Forma_Biologica_1)
-
-
-
-# removing entry mistakes ----------------------------------------------------------
-
-## Estado - should be 32 distinct values
-## Registro - counting individual entries per Site per 'Conglomerado'
-
-
-# Arb.04 ---------------------------------------------------------
-
-### Arb.04 - quick-arrange to check stuff easier
-Arb.04 <- Arb.04 %>%  
-  arrange(Estado, Conglomerado, Sitio, Registro)
-
-### Arb.04 - Estado - initially 33
-Arb.04 <- Arb.04 %>% 
+Arb.04 <- Raw.04 %>%  
+# normalizing names
+  rename(cgl_sit_reg = cgl_sit_arb,
+         CveVeg_S5 = Cve_veg_SV,
+         TipoVeg_S5 = Tipo_veg_SV,
+         NombreComun = NomComun,
+         FormaBiologica = forma_biologica,
+         Distancia = distancia,
+         AlturaTotal = Altura_total,
+         DiametroNormal = Diametro_normal,
+         AreaBasal = Area_basal,
+         AreaCopa = Area_copa,
+         ExposicionCopa = ExposicionLuz,
+         VigorEtapa = VigEtapa,
+         LongitudAnillos10 = Long10Anillos,
+         NumeroAnillos25 = NumAnillos25
+         ) %>% 
+# setting initial column order
+  select(Anio, Estado, Conglomerado, Sitio, Registro, cgl_sit_reg, Arbol, CveVeg_S5, TipoVeg_S5, FormaFuste, 
+         TipoTocon, Familia_APG, NombreCientifico_APG, NombreComun, FormaBiologica, Distancia, Azimut, AlturaTotal,
+         AlturaFusteLimpio, AlturaComercial, DiametroNormal, DiametroBasal, DiametroCopa, AreaBasal, AreaCopa,
+         PosicionCopa, ExposicionCopa, DensidadCopa, TransparenciaCopa, MuerteRegresiva, VigorEtapa, Edad, Condicion,
+         Danio1, Severidad1, Danio2, Severidad2, NumeroTallos, LongitudAnillos10, NumeroAnillos25, GrosorCorteza, X, Y,
+         everything()
+         ) %>%
+# "Estado" Correction - initially 33 -> 32  
   mutate(Estado = case_when(Estado == "Distrito Federal" ~ "Ciudad de México",
-                            TRUE ~ Estado)) %>% 
-  select(Estado, Conglomerado, Sitio, Registro, everything()) %>% 
+                            TRUE ~ Estado),
+  ## "Registro" Correction - initially all values "NULL" -> pull data from string cgl_sit_reg
+         Registro = str_extract(cgl_sit_reg, "(?<=_)[^_]+$") %>%
+           as.integer()) %>%
+# sorting for comparison
   arrange(Estado, Conglomerado, Sitio, Registro)
 
-View(Arb.04)
-
-### Arb.04 - Registro - initially all values 'NULL'
-Arb.04 <- Arb.04 %>% 
-  mutate(Registro = str_extract(string = cgl_sit_arb, "(?<=_)[^_]+$") %>% #extracting values out of (somehow complete) strings
-  as.integer(Registro)) %>% 
-  select(Estado, Conglomerado, Sitio, Registro, everything()) 
-
-View(Arb.04)
 
 
 
@@ -91,54 +89,195 @@ View(Arb.04)
 
 
 
+##----------------------------------------------------------------------------------------------------------------
+## Arb.09 Data cleaning ------------------------------------------------------------------------------------------
 
-
-### Arb.09 - Estado - initially 38
-Arb.09 <- Arb.09 %>% 
+Arb.09 <- Raw.09 %>% 
+# nomralizing names
+  rename(cgl_sit_reg = cgl_sit_arb,
+         CveVeg_S5 = Cve_veg_SV,
+         TipoVeg_S5 = Tipo_veg_SV,
+         NombreComun = NomComun,
+         FormaBiologica = Forma_Biologica_1,
+         Distancia = distancia,
+         AlturaTotal = Altura_total,
+         DiametroNormal = Diametro_normal,
+         AreaBasal = Area_basal,
+         AreaCopa = Area_copa,
+         ExposicionCopa = ExposicionLuz,
+         VigorEtapa = VigEtapa,
+         LongitudAnillos10 = Long10Anillos,
+         NumeroAnillos25 = NumAnillos25
+         ) %>% 
+# "Estado" Correction - initially 38 -> 32  
   mutate(Estado = case_when(Estado == "Distrito Federal" ~ "Ciudad de México",
                             Estado == "Michoacan de Ocampo" ~ "Michoacán de Ocampo",
                             Estado == "Nuevo Leon" ~ "Nuevo León",
                             Estado == "Queretaro de Arteaga" ~ "Querétaro",
                             Estado == "San Luis Potosi" ~ "San Luis Potosí",
                             Estado == "Yucatan" ~ "Yucatán",
-                            TRUE ~ Estado)) %>% 
-  select(Estado, Conglomerado, Sitio, Registro, everything()) %>% 
+                            TRUE ~ Estado)
+         ) %>% 
+# "Registro" Correction + "cgl_sit_arb" Correction - obviously wrong entries -> corrected to expected entry   
+  mutate(Registro = case_when(Registro == 316 ~ 16,
+                              TRUE ~ Registro),
+         cgl_sit_reg = str_replace(string = cgl_sit_reg, "316$", "16"),
+         cgl_sit_reg = str_replace(string = cgl_sit_reg, "4_147$", "4_28"),
+         Registro = ifelse(cgl_sit_reg == "21314_4_28", as.integer(str_extract(cgl_sit_reg, "\\d+$")), Registro)
+         ) %>% 
+# setting initial column order
+  select(Anio, Estado, Conglomerado, Sitio, Registro, cgl_sit_reg, Arbol, CveVeg_S5, TipoVeg_S5, FormaFuste, 
+         TipoTocon, Familia_APG, NombreCientifico_APG, NombreComun, FormaBiologica, Distancia, Azimut, AlturaTotal,
+         AlturaFusteLimpio, AlturaComercial, DiametroNormal, DiametroBasal, DiametroCopa, AreaBasal, AreaCopa,
+         PosicionCopa, ExposicionCopa, DensidadCopa, TransparenciaCopa, MuerteRegresiva, VigorEtapa, Edad, Condicion,
+         Danio1, Severidad1, Danio2, Severidad2, NumeroTallos, LongitudAnillos10, NumeroAnillos25, GrosorCorteza, X, Y,
+         everything() 
+         ) %>% 
+  arrange(Estado, Conglomerado, Sitio, Registro) 
+
+
+
+## Check-up Arb.09
+Arb.09 %>% 
+  distinct(Estado) %>% 
+  count()
+
+max(Arb.09$Registro)
+
+
+
+
+
+##----------------------------------------------------------------------------------------------------------------
+## Arb.14 Data cleaning ------------------------------------------------------------------------------------------
+
+Arb.14 <- Raw.14 %>% 
+# nomralizing names
+  rename(Anio = Anio_C3,
+         Estado = Estado_C3,
+         Conglomerado = IdConglomerado,
+         Sitio = Sitio_C3,
+         Registro = Consecutivo_C3,
+         CveVeg_S7 = CVE_S7_C3,
+         TipoVeg_S7 = DESCRIP_S7_C3,
+         FormaFuste = FormaFuste_C3,
+         TipoTocon = TipoTocon_C3,
+         Familia_APG = Familia_APG_C3,
+         NombreCientifico_APG = NombreCientifico_APG_C3,
+         NombreComun = NombreComun_C3,
+         FormaBiologica = Forma_Biologica_Cat_C3,
+         Distancia = Distancia_C3,
+         Azimut = Azimut_C3,
+         AlturaTotal = AlturaTotal_C3,
+         AlturaFusteLimpio = AlturaFusteLimpio_C3,
+         AlturaComercial = AlturaComercial_C3,
+         DiametroNormal = DiametroNormal_C3,
+         DiametroBasal = DiametroBasalSub_validacion_C3,
+         DiametroCopa = DiametroCopa_C3,
+         AreaBasal = AreaBasal_C3,
+         AreaCopa = AreaCopa_C3,
+         PosicionCopa = PosicionCopa_C3,
+         ExposicionCopa = ExposicionCopa_C3,
+         DensidadCopa = DensidadCopa_C3,
+         TransparenciaCopa = TransparenciaFollaje_C3,
+         MuerteRegresiva = MuerteRegresiva_C3,
+         VigorEtapa = VigorEtapa_C3,
+         Edad = Edad_C3,
+         Condicion = Condicion_C3,
+         Danio1 = AgenteDanio1_C3,
+         Severidad1 = Severidad1_C3,
+         Danio2 = AgenteDanio2_C3,
+         Severidad2 = Severidad2_C3,
+         NumeroTallos = NoRama_C3,
+         LongitudAnillos10 = LongitudAnillos10_C3,
+         NumeroAnillos25 = NumeroAnillos25_C3,
+         GrosorCorteza = GrosorCorteza_C3,
+         Genero_APG = Genero_APG_C3,
+         Especie_APG = Especie_APG_C3,
+         X = X_C3,
+         Y = Y_C3
+         ) %>% 
+# mutate() %>% ### einfügen von cgl_sit_reg   <----- HIER MUSS NOCH WAS REIN
+# setting initial column order
+  select(Anio, Estado, Conglomerado, Sitio, Registro, NoIndividuo_C3, CveVeg_S7, TipoVeg_S7, FormaFuste, TipoTocon, Familia_APG,
+         NombreCientifico_APG, NombreComun, FormaBiologica, Distancia, Azimut, AlturaTotal, AlturaFusteLimpio, AlturaComercial,
+         DiametroNormal, DiametroBasal, DiametroCopa, AreaBasal, AreaCopa, PosicionCopa, ExposicionCopa, DensidadCopa,
+         TransparenciaCopa, MuerteRegresiva, VigorEtapa, Edad, Condicion, Danio1, Severidad1, Danio2, Severidad2, NumeroTallos,
+         LongitudAnillos10, NumeroAnillos25, GrosorCorteza, X, Y,
+         everything()
+         ) %>% 
   arrange(Estado, Conglomerado, Sitio, Registro)
 
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------------------------------------------------------------------
+# PROTOTYPING: REMOVING DATA MISTAKES/INCONSISTENCIES ----------------------------------------------------------
+
+## Estado - should be 32 distinct values
+## Registro - counting individual entries per Site per 'Conglomerado'
+## 
+
+
+
+#-----------------------------------------------------------------------------
+# Arb.04 (TEMPORARY) ---------------------------------------------------------
+View(Arb.04)
+
+# CHANGELOG - Arb.04 ----------------------------------------------------------------------
+### Arb.04 - Estado - initially 33
+### Arb.04 - Registro -  all values "NULL" - pulling data from string cgl_sit_reg
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------------------------------------------------
+# Arb.09 (TEMPORARY) ---------------------------------------------------------
 View(Arb.09)
 
-
+# CHANGELOG - Arb.09 ----------------------------------------------------------------------
+### Arb.09 - Estado - initially 38
 ### Arb.09 - Registro -  Mistake in Guerrero 69336 2: 316 - supposedly just 16
+### Arb.09 - Registro - Mistake in Chihuahua 21314 4: 147 - supposedly just 28
 ### Arb.09 - cgl_sit_arb - same issue in string of Conglomerado, Sitio and Registro
-Test.09 <- Arb.09 %>% 
-  mutate(Registro = case_when(Registro == 316 ~ 16,
-                              TRUE ~ Registro)) %>% 
-  mutate(cgl_sit_arb = str_replace(string = cgl_sit_arb, "316$", "16"))
-
-
-### Arb.09 - Registro - check
-Test.09 <- Arb.09 %>% 
-  distinct(Registro)
-
-View(Test.09)
-
-### Arb.09 - Registro - Mistake in Guerrero 69336 2: 316 - supposedly just 16
-Test.09 <- Arb.09 %>% 
-  mutate(Registro = case_when(Registro == 316 ~ 16,
-                              TRUE ~ Registro)) %>% 
-  mutate(cgl_sit_arb = str_replace(string = cgl_sit_arb, "316$", "16"))
 
 
 
 
 
 
-### Arb.14 - initially 32 
-###-----------------------------------
 
 
 
-# 
+
+
+#-----------------------------------------------------------------------------
+# Arb.14 (TEMPORARY) ---------------------------------------------------------
+View(Arb.09)
+
+# CHANGELOG - Arb.09 ----------------------------------------------------------------------
+### Arb.09 - Estado - initially 38
+### Arb.09 - Registro -  Mistake in Guerrero 69336 2: 316 - supposedly just 16
+### Arb.09 - Registro - Mistake in Chihuahua 21314 4: 147 - supposedly just 28
+### Arb.09 - cgl_sit_arb - same issue in string of Conglomerado, Sitio and Registro
 
 
 
@@ -163,13 +302,8 @@ Test.09 <- Arb.09 %>%
 
 
 
-
-
-
-
-
-
-
+#-----------------------------------------------------------------------------
+# ABSOLUTE TESTING BOOTH ---------------------------------------------------------
 
 # testing and screening of ds
 Test.04 <- Arb.04
@@ -178,71 +312,20 @@ Test.14 <- Arb.14
 
 
 
+# ECO - Formato values
 
-# test for "Estado"
+Arb.04 %>% 
+  select(TransparenciaCopa) %>% 
+  distinct(TransparenciaCopa)
 
-## Arb.04
+Arb.09 %>% 
+  select(TransparenciaCopa) %>% 
+  distinct(TransparenciaCopa)
 
-### First part: normalizing names for estado
-Test.04 <- Test.04 %>% 
-  mutate(Estado = case_when(Estado == "Distrito Federal" ~ "Ciudad de México",
-                            TRUE ~ Estado)) %>% 
-  select(Estado, Conglomerado, Sitio, Registro, everything()) %>% 
-  arrange(Estado, Conglomerado, Sitio, Registro) %>% 
-View(Test.04)
-
-Test.04 <- Test.04 %>% 
-  select(Estado) %>%
-  arrange(Estado)
-  distinct(Estado)
-
-## Arb.09
-
-### First part: normalizing names for estado --> only one missing "Ciudad de Mexico" ~ "Distrito federal"
-Test.09 <- Test.09 %>% 
-  mutate(Estado = case_when(Estado == "Distrito Federal" ~ "Ciudad de México",
-                            Estado == "Michoacan de Ocampo" ~ "Michoacán de Ocampo",
-                            Estado == "Nuevo Leon" ~ "Nuevo León",
-                            Estado == "Queretaro de Arteaga" ~ "Querétaro",
-                            Estado == "San Luis Potosi" ~ "San Luis Potosí",
-                            Estado == "Yucatan" ~ "Yucatán",
-                            TRUE ~ Estado)) %>% 
-  select(Estado, Conglomerado, Sitio, Registro, everything()) %>% 
-  arrange(Estado, Conglomerado, Sitio, Registro) %>% 
-View(Test.09)
-
-
-## Arb.14 
-T.14 <- Arb.14 %>% 
-  arrange(Estado_C3) %>% 
-  distinct(Estado_C3) %>% 
-  mutate(Cve_Estado = row_number())
-View(T.14)
-
-## Difference: Arb.09 has duplicates + "Distro Federal" which is missing in Arb.14
-
-
-Arb.14 <- Arb.14 %>% 
-  arrange(IdConglomerado, Sitio_C3, NoRama_C3)
 Arb.14 %>% 
-  arrange(Cve_Estado_C3) %>% 
-  View()
+  select(Estado_C3, IdConglomerado, Sitio_C3, NoIndividuo_C3, Consecutivo_C3, ArboladoID_C3) %>% 
+  arrange(ArboladoID_C3) %>% 
+  count(ArboladoID_C3)
 
-View(Arb.14)
 
 
-T.09 <- Arb.09 %>%
-  arrange(Conglomerado, Sitio, Registro) %>% 
-  filter(Conglomerado == 153)
-View(T.09)
-
-T.14 <- Arb.14 %>% 
-  arrange(IdConglomerado, Sitio_C3, NoIndividuo_C3) %>% 
-  filter(IdConglomerado == 153)
-View(T.14)
-
-T.14.2 <- Arb.14 %>% 
-  arrange(IdConglomerado, Sitio_C3, NoIndividuo_C3) %>%
-  filter(FAO_S7_C3 != "NA")
-
-View(T.14.2)
