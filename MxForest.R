4be86e2bac9b8dbc38d8f1174020196391a928d1
Sam Huhnke <sam.huhnke@web.de>
2023-08-14 13:58:02 +0200

    added cleaning of several variables of Arb.04 + updated Summary

--- MxForest.R
+++ MxForest.R
@@ -76,9 +76,46 @@ Arb.04 <- Raw.04 %>%
 # "Estado" Correction - initially 33 -> 32  
   mutate(Estado = case_when(Estado == "Distrito Federal" ~ "Ciudad de México",
                             TRUE ~ Estado),
-  ## "Registro" Correction - initially all values "NULL" -> pull data from string cgl_sit_reg
+  # "Registro" Correction - initially all values "NULL" -> pull data from string cgl_sit_reg
          Registro = str_extract(cgl_sit_reg, "(?<=_)[^_]+$") %>%
-           as.integer()) %>%
+           as.integer(),
+  # "AlturaFusteLimpio" Correction - NULL + class "character" ->  NA + class "numeric"
+    AlturaFusteLimpio = ifelse(AlturaFusteLimpio != "NULL", as.numeric(AlturaFusteLimpio), NA),
+  # "AlturaComercial" Correction - NULL + class "character" ->  NA + class "numeric"
+    AlturaComercial = ifelse(AlturaComercial != "NULL", as.numeric(AlturaComercial), NA),
+  # "DiametroBasal" Correction - NULL + class "character" ->  NA + class "numeric"
+    DiametroBasal = ifelse(DiametroBasal != "NULL", as.numeric(DiametroBasal), NA),
+  # "DiametroCopa" Correction - NULL + class "character" ->  NA + class "numeric"
+    DiametroCopa = ifelse(DiametroCopa != "NULL", as.numeric(DiametroCopa), NA),
+  # "AreaBasal" Correction - NULL + class "character" ->  NA + class "numeric"
+    AreaBasal = ifelse(AreaBasal != "NULL", as.numeric(AreaBasal), NA),
+  # "AreaCopa" Correction - NULL + class "character" ->  NA + class "numeric"
+    AreaCopa = ifelse(AreaCopa != "NULL", as.numeric(AreaCopa), NA), 
+  # "VigorEtapa" Correction - NULL + class names -> "no capturado" + new names (in line with Arb.14)
+    VigorEtapa = case_when(VigorEtapa == "Arbol joven" ~ "Árbol joven",
+                           VigorEtapa == "Arbol maduro" ~ "Árbol maduro",
+                           VigorEtapa == "Arbol muy joven" ~ "Árbol muy joven",
+                           VigorEtapa == "Arbol viejo o supermaduro" ~ "Arbol viejo o super-maduro",
+                           VigorEtapa == "NULL" ~ "No capturado",
+                           TRUE ~ VigorEtapa),
+  # "Condicion" Correction - values names -> new value names
+    Condicion = case_when(Condicion == "Muerto en pie" ~ "Árbol muerto en pie",
+                          Condicion == "Vivo" ~ "Árbol vivo",
+                          TRUE ~ Condicion),
+  # "Edad" Correction - "NULL" + class "character" -> NA + class "numeric" (+ rounding numbers)
+    Edad = ifelse(Edad != "NULL",
+                  ifelse(as.numeric(Edad) - floor(as.numeric(Edad)) >= 0.5,
+                         ceiling(as.numeric(Edad)),
+                         floor(as.numeric(Edad))),
+                  NA),
+  # "NumeroTallos" Correction - "NULL" + class "character" -> NA + class "numeric"
+    NumeroTallos = ifelse(NumeroTallos != "NULL", as.numeric(NumeroTallos), NA),
+  # "LongitudAnillos10" Correction - "NULL" + class "character" -> NA + class "numeric"
+    LongitudAnillos10 = ifelse(LongitudAnillos10 != "NULL", as.numeric(LongitudAnillos10), NA),
+  # "NumeroAnillos25" Correction - "NULL" + class "character" -> NA + class "numeric"
+    NumeroAnillos25 = ifelse(NumeroAnillos25 != "NULL", as.numeric(NumeroAnillos25), NA),
+  # "GrosorCorteza" Correction - "NULL" + class "character" -> NA + class "numeric"
+    GrosorCorteza = ifelse(GrosorCorteza != "NULL", as.numeric(GrosorCorteza), NA)) %>% 
 # sorting for comparison
   arrange(Estado, Conglomerado, Sitio, Registro)
 
@@ -236,48 +273,103 @@ View(Arb.04)
 # CHANGELOG - Arb.04 ----------------------------------------------------------------------
 ### Arb.04 - Estado - initially 33 -> changed to 32
 ### Arb.04 - Registro -  all values "NULL" -> pulling data from string cgl_sit_reg
-### Arb.04 - AlturaFusteLimpio - "NULL" + "character" -> "NA" + "numeric"
-### Arb.04 - AlturaComercial - "NULL" + "character" -> "NA" + "numeric"
-### Arb.04 - DiametroBasal - "NULL" + "character" -> "NA" + "numeric"
-### Arb.04 - DiametroCopa - "NULL" + "character" -> "NA" + "numeric"
-### Arb.04 - AreaBasal - "NULL" + "character" -> "NA" + "numeric"
-### Arb.04 - AreaCopa - "NULL" + "character" -> "NA" + "numeric"
+### Arb.04 - AlturaFusteLimpio - "NULL" + class "character" -> "NA" + class "numeric"
+### Arb.04 - AlturaComercial - "NULL" + class "character" -> "NA" + class "numeric"
+### Arb.04 - DiametroBasal - "NULL" + class "character" -> "NA" + class "numeric"
+### Arb.04 - DiametroCopa - "NULL" + class "character" -> "NA" + class "numeric"
+### Arb.04 - AreaBasal - "NULL" + class "character" -> "NA" + class "numeric"
+### Arb.04 - AreaCopa - "NULL" + class "character" -> "NA" + class "numeric"
+### Arb.04 - VigorEtapa - "NULL" + value names -> "no capturado" + new value names
+### Arb.04 - Condicion - value names -> new value names
+### Arb.04 - Edad - "NULL" + class "character" -> NA +  class "numeric" (+ rounding numbers)
+### Arb.04 - NumeroTallos - "NULL" + class "character" -> NA + class "numeric"
+### Arb.04 - LongitudAnillos10 - "NULL" + class "character" -> NA + class "numeric"
+### Arb.04 - NumeroAnillos25 - "NULL" + class "character" -> NA + class "numeric"
+### Arb.04 - GrosorCorteza - "NULL" + class "character" -> NA + class "numeric"
+
+
 
 
 # replacing NULL with NA whilst changing other values from class "character" to class "numeric"
 T.04 <- Arb.04 %>% 
   mutate(
-    # "AlturaFusteLimpio" Correction - NULL + value "character" ->  NA + value class numeric
+    # "AlturaFusteLimpio" Correction - NULL + class "character" ->  NA + class "numeric"
     AlturaFusteLimpio = ifelse(AlturaFusteLimpio != "NULL", as.numeric(AlturaFusteLimpio), NA),
-    # "AlturaComercial" Correction - NULL + value "character" ->  NA + value class numeric
+    # "AlturaComercial" Correction - NULL + class "character" ->  NA + class "numeric"
     AlturaComercial = ifelse(AlturaComercial != "NULL", as.numeric(AlturaComercial), NA),
-    # "DiametroBasal" Correction - NULL + value "character" ->  NA + value class numeric
+    # "DiametroBasal" Correction - NULL + class "character" ->  NA + class "numeric"
     DiametroBasal = ifelse(DiametroBasal != "NULL", as.numeric(DiametroBasal), NA),
-    # "DiametroCopa" Correction - NULL + value "character" ->  NA + value class numeric
+    # "DiametroCopa" Correction - NULL + class "character" ->  NA + class "numeric"
     DiametroCopa = ifelse(DiametroCopa != "NULL", as.numeric(DiametroCopa), NA),
-    # "AreaBasal" Correction - NULL + value "character" ->  NA + value class numeric
+    # "AreaBasal" Correction - NULL + class "character" ->  NA + class "numeric"
     AreaBasal = ifelse(AreaBasal != "NULL", as.numeric(AreaBasal), NA),
-    # "AreaCopa" Correction - NULL + value "character" ->  NA + value class numeric
-    AreaCopa = ifelse(AreaCopa != "NULL", as.numeric(AreaCopa), NA))
+    # "AreaCopa" Correction - NULL + class "character" ->  NA + class "numeric"
+    AreaCopa = ifelse(AreaCopa != "NULL", as.numeric(AreaCopa), NA), 
+    # "VigorEtapa" Correction - NULL + class names -> "no capturado" + new names (in line with Arb.14)
+    VigorEtapa = case_when(VigorEtapa == "Arbol joven" ~ "Árbol joven",
+                                VigorEtapa == "Arbol maduro" ~ "Árbol maduro",
+                                VigorEtapa == "Arbol muy joven" ~ "Árbol muy joven",
+                                VigorEtapa == "Arbol viejo o supermaduro" ~ "Arbol viejo o super-maduro",
+                                VigorEtapa == "NULL" ~ "No capturado",
+                                TRUE ~ VigorEtapa),
+    # "Condicion" Correction - values names -> new value names
+    Condicion = case_when(Condicion == "Muerto en pie" ~ "Árbol muerto en pie",
+                               Condicion == "Vivo" ~ "Árbol vivo",
+                               TRUE ~ Condicion),
+    # "Edad" Correction - "NULL" + class "character" -> NA + class "numeric" (+ rounding numbers)
+    Edad = ifelse(Edad != "NULL",
+                  ifelse(as.numeric(Edad) - floor(as.numeric(Edad)) >= 0.5,
+                         ceiling(as.numeric(Edad)),
+                         floor(as.numeric(Edad))),
+                  NA),
+    # "NumeroTallos" Correction - "NULL" + class "character" -> NA + class "numeric"
+    NumeroTallos = ifelse(NumeroTallos != "NULL", as.numeric(NumeroTallos), NA),
+    # "LongitudAnillos10" Correction - "NULL" + class "character" -> NA + class "numeric"
+    LongitudAnillos10 = ifelse(LongitudAnillos10 != "NULL", as.numeric(LongitudAnillos10), NA),
+    # "NumeroAnillos25" Correction - "NULL" + class "character" -> NA + class "numeric"
+    NumeroAnillos25 = ifelse(NumeroAnillos25 != "NULL", as.numeric(NumeroAnillos25), NA),
+    # "GrosorCorteza" Correction - "NULL" + class "character" -> NA + class "numeric"
+    GrosorCorteza = ifelse(GrosorCorteza != "NULL", as.numeric(GrosorCorteza), NA))
+  
+  
 
-View(T.04)
+
+  
+  # ------- "Edad" Correction - "NULL" + class "character" -> NA + class "numeric" (+ rounding numbers)
+  mutate(Edad = ifelse(Edad != "NULL", 
+                      ifelse(as.numeric(Edad) - floor(as.numeric(Edad)) >= 0.5,
+                             ceiling(as.numeric(Edad)),
+                             floor(as.numeric(Edad))),
+                      NA),
+        ) %>% 
+  
+  # "NumeroTallos" Correction - "NULL" + class "character" -> NA + class "numeric"
+  mutate(NumeroTallos = ifelse(NumeroTallos != "NULL", as.numeric(NumeroTallos), NA)) %>% 
+  # "LongitudAnillos10" Correction - "NULL" + class "character" -> NA + class "numeric"
+  mutate(LongitudAnillos10 = ifelse(LongitudAnillos10 != "NULL", as.numeric(LongitudAnillos10), NA)) %>% 
+  # "NumeroAnillos25" Correction - "NULL" + class "character" -> NA + class "numeric"
+  mutate(NumeroAnillos25 = ifelse(NumeroAnillos25 != "NULL", as.numeric(NumeroAnillos25), NA)) %>% 
+  # "GrosorCorteza" Correction - "NULL" + class "character" -> NA + class "numeric"
+  mutate(GrosorCorteza = ifelse(GrosorCorteza != "NULL", as.numeric(GrosorCorteza), NA))
 
 
-T.04 <- Arb.04 %>%
-  distinct(AreaBasal) %>% 
-  arrange(AreaBasal)
 
-T.04 %>%
-  filter(is.na(AreaCopa)) %>% 
-  count()
-
-Arb.04 %>%
-  filter(AreaCopa == "NULL") %>% 
-  count()
+# Testing ------------------------------------
+# CURRENT: 
 
 
 
-class(T.04$AreaCopa)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -294,19 +386,45 @@ View(Arb.09)
 ### Arb.09 - Registro - Mistake in Chihuahua 21314 4: 147 - supposedly just 28
 ### Arb.09 - cgl_sit_arb - same issue in string of Conglomerado, Sitio and Registro
 
+T.09 <- Arb.09 %>%
+  distinct(Edad) %>% 
+  arrange(Edad)
 
+View(T.09)
 
+Age.09 <- Arb.09 %>% 
+  mutate(Int = ifelse(Edad != "NULL", as.integer(Edad), NA),
+         Num = ifelse(Edad != "NULL", as.numeric(Edad), NA)) %>% 
+  select(Edad, Int, Num) %>% 
+  filter(!is.na(Int)) %>% 
+  arrange(Int)
 
+# -0.098 neben orginal wert
+mean(Age.09$Int) #-
+mean(Age.09$Num)
+# +0.008 neben orginal wert
 
+Age.09 <- Arb.09 %>%
+  mutate(Int = ifelse(Edad != "NULL", 
+                      ifelse(as.numeric(Edad) - floor(as.numeric(Edad)) >= 0.5,
+                             ceiling(as.numeric(Edad)),
+                             floor(as.numeric(Edad))),
+                      NA),
+         Num = ifelse(Edad != "NULL", as.numeric(Edad), NA)) %>% 
+  select(Edad, Int, Num) %>% 
+  filter(!is.na(Int)) %>% 
+  distinct() %>% 
+  arrange(Int)
 
+View(Age.09)
 
-
+mean(Age.09$Int) - mean(Age.09$Num)
 
 
 
 #-----------------------------------------------------------------------------
 # Arb.14 (TEMPORARY) ---------------------------------------------------------
-View(Arb.09)
+View(Arb.14)
 
 # CHANGELOG - Arb.09 ----------------------------------------------------------------------
 ### Arb.09 - Estado - initially 38
@@ -316,9 +434,13 @@ View(Arb.09)
 
 
 
+T.14 <- Arb.14 %>%
+  distinct(Edad) %>% 
+  arrange(Edad)
 
+View(T.14)
 
-
+class(Arb.14$Edad)
 
 
 
@@ -376,9 +498,9 @@ Arb.09 %>%
 
 # Arb.14 --------------------
 
-T.14 <- Arb.14 %>%
-  distinct(MuerteRegresiva) %>% 
-  arrange(MuerteRegresiva)
+Arb.14 %>%
+  distinct(VigorEtapa) %>% 
+  arrange(VigorEtapa)
 
 View(T.14)
 
