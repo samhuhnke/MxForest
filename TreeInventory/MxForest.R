2efbc32acc69ce0a10b10f8413bdca0033ecb55d
Sam Huhnke <sam.huhnke@web.de>
2024-02-08 11:03:16 +0100

    recalculated iMAD results with NAs due to issues with other database

--- TreeInventory/MxForest.R
+++ TreeInventory/MxForest.R
@@ -924,6 +924,7 @@ FullStack_V3 <- FullStack_V2 |>
 
 
 ################### 4) RE-INTRODUCE ECOREGIONS, TOTAL ENTRIES, SPEC COUNT, AVG DBH PER CLUSTER FOR EACH CYCLE ---------
+#### 4.1) FUllStack_V4 ----
 FullStack_V4 <- FullStack_V3 |> 
   left_join(Sec.14 |> 
               mutate(Cluster_ID = IDConglomerado) |> 
@@ -989,42 +990,7 @@ FullStack_V4 <- FullStack_V3 |>
          SC2 = case_when(Muestreado2 == 1 & is.na(SC2) ~ 0,
                          T ~ SC2),
          SC3 = case_when(Muestreado3 == 1 & is.na(SC3) ~ 0,
-                         T ~ SC3),
-         # DBH
-         DBH1 = case_when(Muestreado1 == 1 & is.na(DBH1) ~ 0,
-                         T ~ DBH1),
-         DBH2 = case_when(Muestreado2 == 1 & is.na(DBH2) ~ 0,
-                         T ~ DBH2),
-         DBH3 = case_when(Muestreado3 == 1 & is.na(DBH3) ~ 0,
-                         T ~ DBH3),
-         # Crown Diameter
-         CD1 = case_when(Muestreado1 == 1 & is.na(CD1) ~ 0,
-                          T ~ CD1),
-         CD2 = case_when(Muestreado2 == 1 & is.na(CD2) ~ 0,
-                          T ~ CD2),
-         CD3 = case_when(Muestreado3 == 1 & is.na(CD3) ~ 0,
-                          T ~ CD3),
-         # Crown Height
-         CH1 = case_when(Muestreado1 == 1 & is.na(CH1) ~ 0,
-                          T ~ CH1),
-         CH2 = case_when(Muestreado2 == 1 & is.na(CH2) ~ 0,
-                          T ~ CH2),
-         CH3 = case_when(Muestreado3 == 1 & is.na(CH3) ~ 0,
-                          T ~ CH3),
-         # Crown Area
-         CA1 = case_when(Muestreado1 == 1 & is.na(CA1) ~ 0,
-                         T ~ CA1),
-         CA2 = case_when(Muestreado2 == 1 & is.na(CA2) ~ 0,
-                         T ~ CA2),
-         CA3 = case_when(Muestreado3 == 1 & is.na(CA3) ~ 0,
-                         T ~ CA3),
-         # Tree Height
-         TH1 = case_when(Muestreado1 == 1 & is.na(CA1) ~ 0,
-                         T ~ CA1),
-         TH2 = case_when(Muestreado2 == 1 & is.na(CA2) ~ 0,
-                         T ~ CA2),
-         TH3 = case_when(Muestreado3 == 1 & is.na(CA3) ~ 0,
-                         T ~ CA3),
+                         T ~ SC3)
          ) |> 
   # in case you wanted to calculate univariate changes in R 
   mutate(CTE13 = TE3 - TE1,
@@ -1032,6 +998,114 @@ FullStack_V4 <- FullStack_V3 |>
          DBH13 = DBH3 - DBH1) |> 
   ungroup()
 
+#### 4.2) FullStack_V4_Zeros ----
+FullStack_V4_Zeros <- FullStack_V3 |> 
+  left_join(Sec.14 |> 
+              mutate(Cluster_ID = IDConglomerado) |> 
+              select(Cluster_ID, DESECON1_C3, DESECON2_C3, DESECON3_C3, DESECON4_C3),
+            by = "Cluster_ID") |> 
+  left_join(Comp_C_Diagnostics_V5 |> 
+              ungroup() |> 
+              filter(File == 1) |> 
+              mutate(Cluster_ID2 = Cluster_ID,
+                     Cluster_ID = Conglomerado,
+                     DBH1 = AvgDbh,
+                     CD1 = AvgCrownDiameter,
+                     CH1 = AvgCrownHeight,
+                     CA1 = AvgCrownArea,
+                     SC1 = species_count,
+                     TH1 = AvgTreeHeight,
+                     TE1 = total_entries,
+                     J1 = J) |> 
+              select(Cluster_ID, DBH1, CD1, CH1, CA1, SC1, TH1, TE1, J1),
+            by = "Cluster_ID") |> 
+  left_join(Comp_C_Diagnostics_V5 |> 
+              ungroup() |> 
+              filter(File == 2) |> 
+              mutate(Cluster_ID2 = Cluster_ID,
+                     Cluster_ID = Conglomerado,
+                     DBH2 = AvgDbh,
+                     CD2 = AvgCrownDiameter,
+                     CH2 = AvgCrownHeight,
+                     CA2 = AvgCrownArea,
+                     SC2 = species_count,
+                     TH2 = AvgTreeHeight,
+                     TE2 = total_entries,
+                     J2 = J) |> 
+              select(Cluster_ID, DBH2, CD2, CH2, CA2, SC2, TH2, TE2, J2),
+            by = "Cluster_ID") |> 
+  left_join(Comp_C_Diagnostics_V5 |> 
+              ungroup() |> 
+              filter(File == 3) |> 
+              mutate(Cluster_ID2 = Cluster_ID,
+                     Cluster_ID = Conglomerado,
+                     DBH3 = AvgDbh,
+                     CD3 = AvgCrownDiameter,
+                     CH3 = AvgCrownHeight,
+                     CA3 = AvgCrownArea,
+                     SC3 = species_count,
+                     TH3 = AvgTreeHeight,
+                     TE3 = total_entries,
+                     J3 = J) |> 
+              select(Cluster_ID, DBH3, CD3, CH3, CA3, SC3, TH3, TE3, J3),
+            by = "Cluster_ID") |> 
+  # Exchanging NAs with Zeros (except for J)
+  mutate(
+    # Tree Entries
+    TE1 = case_when(Muestreado1 == 1 & is.na(TE1) ~ 0,
+                    T ~ TE1),
+    TE2 = case_when(Muestreado2 == 1 & is.na(TE2) ~ 0,
+                    T ~ TE2),
+    TE3 = case_when(Muestreado3 == 1 & is.na(TE3) ~ 0,
+                    T ~ TE3),
+    # Species Count
+    SC1 = case_when(Muestreado1 == 1 & is.na(SC1) ~ 0,
+                    T ~ SC1),
+    SC2 = case_when(Muestreado2 == 1 & is.na(SC2) ~ 0,
+                    T ~ SC2),
+    SC3 = case_when(Muestreado3 == 1 & is.na(SC3) ~ 0,
+                    T ~ SC3),
+    # DBH
+    DBH1 = case_when(Muestreado1 == 1 & is.na(DBH1) ~ 0,
+                     T ~ DBH1),
+    DBH2 = case_when(Muestreado2 == 1 & is.na(DBH2) ~ 0,
+                     T ~ DBH2),
+    DBH3 = case_when(Muestreado3 == 1 & is.na(DBH3) ~ 0,
+                     T ~ DBH3),
+    # Crown Diameter
+    CD1 = case_when(Muestreado1 == 1 & is.na(CD1) ~ 0,
+                    T ~ CD1),
+    CD2 = case_when(Muestreado2 == 1 & is.na(CD2) ~ 0,
+                    T ~ CD2),
+    CD3 = case_when(Muestreado3 == 1 & is.na(CD3) ~ 0,
+                    T ~ CD3),
+    # Crown Height
+    CH1 = case_when(Muestreado1 == 1 & is.na(CH1) ~ 0,
+                    T ~ CH1),
+    CH2 = case_when(Muestreado2 == 1 & is.na(CH2) ~ 0,
+                    T ~ CH2),
+    CH3 = case_when(Muestreado3 == 1 & is.na(CH3) ~ 0,
+                    T ~ CH3),
+    # Crown Area
+    CA1 = case_when(Muestreado1 == 1 & is.na(CA1) ~ 0,
+                    T ~ CA1),
+    CA2 = case_when(Muestreado2 == 1 & is.na(CA2) ~ 0,
+                    T ~ CA2),
+    CA3 = case_when(Muestreado3 == 1 & is.na(CA3) ~ 0,
+                    T ~ CA3),
+    # Tree Height
+    TH1 = case_when(Muestreado1 == 1 & is.na(CA1) ~ 0,
+                    T ~ CA1),
+    TH2 = case_when(Muestreado2 == 1 & is.na(CA2) ~ 0,
+                    T ~ CA2),
+    TH3 = case_when(Muestreado3 == 1 & is.na(CA3) ~ 0,
+                    T ~ CA3),
+  ) |> 
+  # in case you wanted to calculate univariate changes in R 
+  mutate(CTE13 = TE3 - TE1,
+         SC13 = SC3 - SC1,
+         DBH13 = DBH3 - DBH1) |> 
+  ungroup()
 
 
 ################### 4) TREE COUNT CHANGE CALCULATION BASED ON ECOREGIONS -----------------------------------
@@ -1066,9 +1140,6 @@ FullStack_V4 <- FullStack_V3 |>
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 time.taken
-
-
-View(FullStack_V4)
 
 
 ####################          IR-MAD CHANGE DETECTION PREPARATION            ----------------------------------
@@ -1118,7 +1189,55 @@ file12 <- rbind(file1, file2)
 
 #### STEP 3: write .csv for python ----
 
-# write.csv(file12, "iMAD_Data_12_Constant.csv")
+ write.csv(file12, "iMAD_Data_12_Constant.csv")
+
+################## 1.1) ZEROs - Comparison of Cycle 1 and 2 - FILTER: Only Clusters that are available for 1 and 2 --------------------------------------------
+#### STEP 1: dissecting dataframe by file + add file grouping variable (CONSTANT CLUSTERS) -----
+## File 1
+file1 <- FullStack_V4 |> 
+  filter(Muestreado1 == 1 & Muestreado2 == 1) |> 
+  select(Cluster_ID, DBH1, CD1, CH1, CA1, SC1, TH1, TE1, J1, X, Y) |> 
+  # changing variable names to file-unspecific names -> in order to create long data format 
+  rename(AvgDbh = DBH1,
+         AvgCrownDiameter = CD1,
+         AvgCrownHeight = CH1,
+         AvgCrownArea = CA1,
+         SpeciesCount = SC1,
+         AvgTreeHeight = TH1,
+         TreeCount = TE1,
+         J = J1) |> 
+  # add file grouping
+  mutate(File = 1) |> 
+  relocate(File)
+
+file1
+
+## File 2
+file2 <- FullStack_V4 |> 
+  filter(Muestreado1 == 1 & Muestreado2 == 1) |> 
+  select(Cluster_ID, DBH2, CD2, CH2, CA2, SC2, TH2, TE2, J2, X, Y) |> 
+  # changing variable names to file-unspecific names -> in order to create long data format 
+  rename(AvgDbh = DBH2,
+         AvgCrownDiameter = CD2,
+         AvgCrownHeight = CH2,
+         AvgCrownArea = CA2,
+         SpeciesCount = SC2,
+         AvgTreeHeight = TH2,
+         TreeCount = TE2,
+         J = J2) |> 
+  #add file grouping
+  mutate(File = 2) |> 
+  relocate(File)
+
+file2
+
+#### STEP 2: merge into long data format ----
+file12 <- rbind(file1, file2)
+
+
+#### STEP 3: write .csv for python ----
+
+ write.csv(file12, "iMAD_Data_12_Constant_Zeros.csv")
 
 ########### CUT -------------------------------------------------------------------------------------------------
 
@@ -1170,8 +1289,55 @@ file23 <- rbind(file2, file3)
 
  write.csv(file23, "iMAD_Data_23_Constant.csv")
 
-########### CUT -------------------------------------------------------------------------------------------------
+################## 2) Comparison of Cycle 2 and 3 - FILTER: Only Clusters that are available for 2 and 3 --------------------------------------------
+#### STEP 1: dissecting dataframe by file + add file grouping variable (CONSTANT CLUSTERS) -----
+## File 2
+file2 <- FullStack_V4_Zeros |> 
+  filter(Muestreado2 == 1 & Muestreado3 == 1) |> 
+  select(Cluster_ID, DBH2, CD2, CH2, CA2, SC2, TH2, TE2, J2, X, Y) |> 
+  # changing variable names to file-unspecific names -> in order to create long data format 
+  rename(AvgDbh = DBH2,
+         AvgCrownDiameter = CD2,
+         AvgCrownHeight = CH2,
+         AvgCrownArea = CA2,
+         SpeciesCount = SC2,
+         AvgTreeHeight = TH2,
+         TreeCount = TE2,
+         J = J2) |> 
+  #add file grouping
+  mutate(File = 2) |> 
+  relocate(File)
 
+file2
+
+## File 3
+file3 <- FullStack_V4_Zeros |> 
+  filter(Muestreado2 == 1 & Muestreado3 == 1) |> 
+  select(Cluster_ID, DBH3, CD3, CH3, CA3, SC3, TH3, TE3, J3, X, Y) |> 
+  # changing variable names to file-unspecific names -> in order to create long data format 
+  rename(AvgDbh = DBH3,
+         AvgCrownDiameter = CD3,
+         AvgCrownHeight = CH3,
+         AvgCrownArea = CA3,
+         SpeciesCount = SC3,
+         AvgTreeHeight = TH3,
+         TreeCount = TE3,
+         J = J3) |> 
+  #add file grouping
+  mutate(File = 3) |> 
+  relocate(File)
+
+file3
+
+#### STEP 2: merge into long data format ----
+file23 <- rbind(file2, file3)
+
+
+#### STEP 3: write .csv for python ----
+
+ write.csv(file23, "iMAD_Data_23_Constant_Zeros.csv")
+
+########### CUT -------------------------------------------------------------------------------------------------
 
 ################## 3) Comparison of Cycle 1 and 3 - FILTER: Only Clusters that are available for 1 and 3 --------------------------------------------
 #### STEP 1: dissecting dataframe by file + add file grouping variable (CONSTANT CLUSTERS) -----
@@ -1221,7 +1387,59 @@ file13 <- rbind(file1, file3)
 
  write.csv(file13, "iMAD_Data_13_Constant.csv")
 
+################## 3.1) ZEROs - Comparison of Cycle 1 and 3 - FILTER: Only Clusters that are available for 1 and 3 --------------------------------------------
+#### STEP 1: dissecting dataframe by file + add file grouping variable (CONSTANT CLUSTERS) -----
+## File 1
+file1 <- FullStack_V4_Zeros |> 
+  filter(Muestreado1 == 1 & Muestreado3 == 1) |> 
+  select(Cluster_ID, DBH1, CD1, CH1, CA1, SC1, TH1, TE1, J1, X, Y) |> 
+  # changing variable names to file-unspecific names -> in order to create long data format 
+  rename(AvgDbh = DBH1,
+         AvgCrownDiameter = CD1,
+         AvgCrownHeight = CH1,
+         AvgCrownArea = CA1,
+         SpeciesCount = SC1,
+         AvgTreeHeight = TH1,
+         TreeCount = TE1,
+         J = J1) |> 
+  # add file grouping
+  mutate(File = 1) |> 
+  relocate(File)
+
+file1
+
+## File 3
+file3 <- FullStack_V4_Zeros |> 
+  filter(Muestreado1 == 1 & Muestreado3 == 1) |> 
+  select(Cluster_ID, DBH3, CD3, CH3, CA3, SC3, TH3, TE3, J3, X, Y) |> 
+  # changing variable names to file-unspecific names -> in order to create long data format 
+  rename(AvgDbh = DBH3,
+         AvgCrownDiameter = CD3,
+         AvgCrownHeight = CH3,
+         AvgCrownArea = CA3,
+         SpeciesCount = SC3,
+         AvgTreeHeight = TH3,
+         TreeCount = TE3,
+         J = J3) |> 
+  #add file grouping
+  mutate(File = 3) |> 
+  relocate(File)
+
+file3
+
+#### STEP 2: merge into long data format ----
+file13 <- rbind(file1, file3)
+
+
+#### STEP 3: write .csv for python ----
+
+ write.csv(file13, "iMAD_Data_13_Constant_Zeros.csv")
+
 ########### END -------------------------------------------------------------------------------------------------
+
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
 
 
 ####################          IR-MAD CHANGE DETECTION RESULTS            ----------------------------------
@@ -1231,43 +1449,23 @@ iMAD_results_12_Constant <- Raw.04 <- fread(here("data", "iMAD", "[1] Cluster", 
 #### STEP 2: Geospatial Prep ----
 # writeVector(vect(iMAD_results_12_Constant, geom = c("X", "Y"), crs = "+proj=longlat +datum=WGS84"), "iMAD_results_12_Constant.shp")
 
-################## 2) Comparison of Cycle 1 and 2 --------------------------------------------
-#### STEP 1: Load data ----
-iMAD_results_12 <- Raw.04 <- fread(here("data", "iMAD", "[1] Cluster", "iMAD_results_12.csv"))
-#### STEP 2: Geospatial Prep ----
-# writeVector(vect(iMAD_results_12, geom = c("X", "Y"), crs = "+proj=longlat +datum=WGS84"), "iMAD_results_12.shp")
-
 ########### CUT -------------------------------------------------------------------------------------------------
 
-################## 1) Comparison of Cycle 2 and 3 - FILTER: CONSTANT CLUSTERS -------------------------------------------
+################## 2) Comparison of Cycle 2 and 3 - FILTER: CONSTANT CLUSTERS -------------------------------------------
 #### STEP 1: Load data ----
 iMAD_results_23_Constant <- Raw.04 <- fread(here("data", "iMAD", "[1] Cluster", "iMAD_results_23_Constant.csv"))
 #### STEP 2: Geospatial Prep ----
 
 # writeVector(vect(iMAD_results_23_Constant, geom = c("X", "Y"), crs = "+proj=longlat +datum=WGS84"), "iMAD_results_23_Constant.shp")
 
-################## 2) Comparison of Cycle 2 and 3 --------------------------------------------
-#### STEP 1: Load data ----
-iMAD_results_23 <- Raw.04 <- fread(here("data", "iMAD", "[1] Cluster", "iMAD_results_23.csv"))
-#### STEP 2: Geospatial Prep ----
-
-# writeVector(vect(iMAD_results_23, geom = c("X", "Y"), crs = "+proj=longlat +datum=WGS84"), "iMAD_results_23.shp")
-
 ########### CUT -------------------------------------------------------------------------------------------------
 
-################## 1) Comparison of Cycle 1 and 3 - FILTER: CONSTANT CLUSTERS -------------------------------------------
+################## 3) Comparison of Cycle 1 and 3 - FILTER: CONSTANT CLUSTERS -------------------------------------------
 #### STEP 1: Load data ----
 iMAD_results_13_Constant <- Raw.04 <- fread(here("data", "iMAD", "[1] Cluster", "iMAD_results_13_Constant.csv"))
 #### STEP 2: Geospatial Prep ----
 
 # writeVector(vect(iMAD_results_13_Constant, geom = c("X", "Y"), crs = "+proj=longlat +datum=WGS84"), "iMAD_results_13_Constant.shp")
-
-################## 2) Comparison of Cycle 1 and 3 --------------------------------------------
-#### STEP 1: Load data ----
-iMAD_results_13 <- Raw.04 <- fread(here("data", "iMAD", "[1] Cluster", "iMAD_results_13.csv"))
-#### STEP 2: Geospatial Prep ----
-
-# writeVector(vect(iMAD_results_13, geom = c("X", "Y"), crs = "+proj=longlat +datum=WGS84"), "iMAD_results_13.shp")
 
 ########### CUT -------------------------------------------------------------------------------------------------
 
